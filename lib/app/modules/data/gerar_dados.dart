a5e1e3e3e99f265f6d3e3a300ad53c1e8e3eedb2
Lourival Vicente <lourivalvsj@gmail.com>
2023-06-13 21:32:05 -0300

    alfabeto Avancado

--- lib/app/modules/data/gerar_dados.dart
+++ lib/app/modules/data/gerar_dados.dart
@@ -1155,6 +1155,456 @@ class GerarDados {
         ],
         createdAt: DateTime.now(),
       ),
+            Pergunta( //albeto c
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetoc.png?alt=media&token=2ee097d6-5494-4747-a9fe-d2ed41defe99",
+        opcoes: [
+          "LETRA D",
+          "LETRA B",
+          "LETRA C",
+          "LETRA A",
+        ],
+        respostaCorreta: "LETRA C",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto D
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetod.png?alt=media&token=9607155c-f38f-4db8-874b-2bdaaadc34e8",
+        opcoes: [
+          "LETRA H",
+          "LETRA E",
+          "LETRA C",
+          "LETRA D",
+        ],
+        respostaCorreta: "LETRA D",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto E
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetoe.png?alt=media&token=1184de54-82ff-4c2e-affa-c5095fb6f684",
+        opcoes: [
+          "LETRA F",
+          "LETRA E",
+          "LETRA C",
+          "LETRA D",
+        ],
+        respostaCorreta: "LETRA E",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto F
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetof.png?alt=media&token=1438140e-351c-48d8-844d-3050340ff734",
+        opcoes: [
+          "LETRA H",
+          "LETRA F",
+          "LETRA C",
+          "LETRA I",
+        ],
+        respostaCorreta: "LETRA F",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto G
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetog.png?alt=media&token=00fbf674-ca9d-4c53-97db-0183bf900016",
+        opcoes: [
+          "LETRA H",
+          "LETRA P",
+          "LETRA C",
+          "LETRA G",
+        ],
+        respostaCorreta: "LETRA G",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto H
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetoh.png?alt=media&token=3d795db5-4c53-4197-af8b-136c526230cc",
+        opcoes: [
+          "LETRA M",
+          "LETRA H",
+          "LETRA C",
+          "LETRA N",
+        ],
+        respostaCorreta: "LETRA H",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto I
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetoi.png?alt=media&token=592809cf-90e0-4fe9-a83a-f16806b5c2bf",
+        opcoes: [
+          "LETRA O",
+          "LETRA F",
+          "LETRA C",
+          "LETRA I",
+        ],
+        respostaCorreta: "LETRA I",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto J
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetoj.png?alt=media&token=5deacded-d555-4429-9377-df41e2e6f155",
+        opcoes: [
+          "LETRA J",
+          "LETRA K",
+          "LETRA X",
+          "LETRA D",
+        ],
+        respostaCorreta: "LETRA J",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto L
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetol.png?alt=media&token=06d5149b-3858-4dd3-aff2-78be0b4e9f87",
+        opcoes: [
+          "LETRA W",
+          "LETRA K",
+          "LETRA L",
+          "LETRA F",
+        ],
+        respostaCorreta: "LETRA L",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto M
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetom.png?alt=media&token=053407b6-bd39-47ab-bffd-bc341f7b3d23",
+        opcoes: [
+          "LETRA Y",
+          "LETRA M",
+          "LETRA Ç",
+          "LETRA V",
+        ],
+        respostaCorreta: "LETRA M",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto N
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabeton.png?alt=media&token=599b9af7-7445-44ca-a08b-4ac27b837f18",
+        opcoes: [
+          "LETRA W",
+          "LETRA S",
+          "LETRA N",
+          "LETRA U",
+        ],
+        respostaCorreta: "LETRA N",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto O
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetoo.png?alt=media&token=bb3efcea-8fcd-43a3-8e9f-d90b2b1d9f07",
+        opcoes: [
+          "LETRA R",
+          "LETRA T",
+          "LETRA C",
+          "LETRA O",
+        ],
+        respostaCorreta: "LETRA O",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto P
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetop.png?alt=media&token=8e05137d-2a6d-4bdc-a03f-9d29081d86ec",
+        opcoes: [
+          "LETRA X",
+          "LETRA Y",
+          "LETRA F",
+          "LETRA P",
+        ],
+        respostaCorreta: "LETRA P",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto Q
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetoq.png?alt=media&token=cddab2fe-5088-4a54-b1ad-aa8a7e551ae1",
+        opcoes: [
+          "LETRA T",
+          "LETRA E",
+          "LETRA C",
+          "LETRA Q",
+        ],
+        respostaCorreta: "LETRA Q",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto R
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetor.png?alt=media&token=a86e261d-cd5c-49f4-8f30-f25e44da14b0",
+        opcoes: [
+          "LETRA J",
+          "LETRA R",
+          "LETRA K",
+          "LETRA V",
+        ],
+        respostaCorreta: "LETRA R",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ), 
+            Pergunta( //albeto T
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetot.png?alt=media&token=a7bc3e94-fbed-4225-8189-623809122cc3",
+        opcoes: [
+          "LETRA W",
+          "LETRA G",
+          "LETRA K",
+          "LETRA T",
+        ],
+        respostaCorreta: "LETRA T",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ), 
+            Pergunta( //albeto U
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetou.png?alt=media&token=4da868bd-5649-4c2f-bef3-713a78121b38",
+        opcoes: [
+          "LETRA P",
+          "LETRA U",
+          "LETRA X",
+          "LETRA D",
+        ],
+        respostaCorreta: "LETRA U",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto W
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetow.png?alt=media&token=b17daab2-a8de-4c46-964c-1dd85cf7ab12",
+        opcoes: [
+          "LETRA B",
+          "LETRA E",
+          "LETRA W",
+          "LETRA N",
+        ],
+        respostaCorreta: "LETRA W",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto X
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetox.png?alt=media&token=30cf38cd-f2cf-421a-b6aa-14b473493164",
+        opcoes: [
+          "LETRA H",
+          "LETRA Y",
+          "LETRA F",
+          "LETRA X",
+        ],
+        respostaCorreta: "LETRA X",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto Y
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetoy.png?alt=media&token=898387e0-4423-48e8-939b-0f41e407f40d",
+        opcoes: [
+          "LETRA Q",
+          "LETRA Y",
+          "LETRA T",
+          "LETRA D",
+        ],
+        respostaCorreta: "LETRA Y",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto Z
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetoz.png?alt=media&token=718265fa-8e22-4c67-ab78-d348017de67b",
+        opcoes: [
+          "LETRA X",
+          "LETRA B",
+          "LETRA C",
+          "LETRA Z",
+        ],
+        respostaCorreta: "LETRA Z",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto K
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetok.png?alt=media&token=315daf39-501a-4063-8528-0d4a79cfb8fe",
+        opcoes: [
+          "LETRA K",
+          "LETRA E",
+          "LETRA U",
+          "LETRA Z",
+        ],
+        respostaCorreta: "LETRA K",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto S
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetos.png?alt=media&token=2fb275cc-9ade-4d34-bc7a-c41b59abebc1",
+        opcoes: [
+          "LETRA S",
+          "LETRA P",
+          "LETRA C",
+          "LETRA W",
+        ],
+        respostaCorreta: "LETRA S",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ), 
+            Pergunta( //albeto V
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabetov.png?alt=media&token=8f6508a1-4d82-48ff-acfe-6a546ced9c97",
+        opcoes: [
+          "LETRA T",
+          "LETRA S",
+          "LETRA X",
+          "LETRA V",
+        ],
+        respostaCorreta: "LETRA V",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
+            Pergunta( //albeto Ç
+        imagem:
+            "https://firebasestorage.googleapis.com/v0/b/ranck-game-libras.appspot.com/o/alfabeto%2Falfabeto%C3%A7.png?alt=media&token=f388c7bc-0242-41b5-9c2c-62fc572f6622",
+        opcoes: [
+          "LETRA C",
+          "LETRA B",
+          "LETRA Ç",
+          "LETRA O",
+        ],
+        respostaCorreta: "LETRA Ç",
+        score: 3,
+        nivelDificuldade: nivelAvancado,
+        modalidades: [
+          modalidadeAlfabeto,
+          modalidadeMisto,
+        ],
+        createdAt: DateTime.now(),
+      ),
       //Pergunta Numeral Basico
       Pergunta( //Numeral 0
         imagem:
